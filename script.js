8cf123c6a78e9b232e97b6d6683c7ab68cadab64
igor-veselkov <veselkov_igor@bk.ru>
2022-08-16 22:51:20 +0500

    added instructions

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
 for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', '');
-  const b = prompt('На сколько оцените его?', '');
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
 
-  personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
